18c3a5c1d416ff311cbe4cca8f76df98835448f6
YellowFishXS <133674460+YellowFishXS@users.noreply.github.com>
2025-11-13 21:22:28 +0800

    Visitor class

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,3 +1,34 @@
-public class Visitor {
+public class Visitor extends Person {
+    //TravelGroup,
+    //e.g. independent travel, China International Travel Service(CITS), Wing On Travel(WOT),Miramar Travel(MT)
+    private String travelGroup; //旅行团
+    private String ticketType;
 
+    public Visitor() {
+        super(); // 调用父类默认构造器
+        this.travelGroup  = "CITS";
+        this.ticketType = "Standard";
+    }
+
+    public Visitor(String name, int age, String sex, String travelGroup , String ticketType) {
+        super(name, sex, age); // 调用父类带参数构造器
+        this.travelGroup  = travelGroup ;
+        this.ticketType = ticketType;
+    }
+
+    public String gettravelGroup () {
+        return travelGroup ;
+    }
+
+    public void settravelGroup (String travelGroup ) {
+        this.travelGroup  = travelGroup ;
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
+    }
 }
