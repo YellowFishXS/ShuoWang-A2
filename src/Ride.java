e92f8f7cd91f6e1da01b34d4c13c18707add192a
YellowFishXS <133674460+YellowFishXS@users.noreply.github.com>
2025-11-13 22:22:58 +0800

    Ride class

--- src/Ride.java
+++ src/Ride.java
@@ -1,3 +1,42 @@
 public class Ride {
+    private Employee operator; 
+    private boolean rideStatus;
+    private int rideSize;
 
+
+    public Ride() {
+        this.rideStatus = false;
+        this.rideSize = 0;
+        this.operator = new Employee(); // default Employee object
+    }
+
+    public Ride(boolean rideStatus, int rideSize, Employee operator) {
+        this.rideStatus = rideStatus;
+        this.rideSize = rideSize;
+        this.operator = operator;
+    }
+
+    public boolean getRideStatus() {
+        return rideStatus;
+    }
+
+    public void setRideStatus(boolean rideStatus) {
+        this.rideStatus = rideStatus;
+    }
+
+    public int getRideSize() {
+        return rideSize;
+    }
+
+    public void setRideSize(int rideSize) {
+        this.rideSize = rideSize;
+    }
+
+    public Employee getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Employee operator) {
+        this.operator = operator;
+    }
 }
