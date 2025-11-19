dd35c20cbca77781394b86b17fc2902acd007958
YellowFishXS <133674460+YellowFishXS@users.noreply.github.com>
2025-11-19 18:46:25 +0800

    visitor compare

---
+++ src/VisitorComparator.java
@@ -0,0 +1,21 @@
+import java.util.Comparator;
+
+public class VisitorComparator implements Comparator<Visitor> {
+
+    @Override
+    public int compare(Visitor visitor1, Visitor visitor2){
+        
+        //sort by age, descending
+        int ageComparison = Integer.compare(visitor2.getAge(), visitor1.getAge());
+        if (ageComparison != 0) {
+            return ageComparison;
+        }
+        
+        //sort by age, ascending
+        return visitor1.getName().compareTo(visitor2.getName());
+    }
+
+
+
+    
+}
